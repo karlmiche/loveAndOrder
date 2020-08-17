974965afdef84e0e7bcffe06670ebb73c0654e06
karlmiche <karlymism@gmail.com>
2020-08-17 13:54:04 -0700

    some more styling

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -2,13 +2,11 @@ import React from 'react';
 import './App.css';
 import PoemContainer from './components/PoemContainer';
 import ChoosePoem from './components/ChoosePoem';
-import ChooseArticle from './components/ChooseArticle';
 
 function App() {
   return (
     <div className="App">
       <PoemContainer />
-      <ChooseArticle />
     </div>
   );
 }
