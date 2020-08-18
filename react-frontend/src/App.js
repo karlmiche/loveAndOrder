243ee828a4fc566c3e37ed9678370cd4a2fea499
karlmiche <karlymism@gmail.com>
2020-08-17 20:36:11 -0700

    good progress with scrolltrigger

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
 import PoemContainer from './components/PoemContainer';
-import Title from './components/title'
-;
+import Title from './components/title';
+// import WhatIsThis from './components/Wtf';
+
 function App() {
   return (
-    <div className="App">
-      <Title />
-      <PoemContainer />
+    <div>
+      <div className="App">
+        <Title />
+        <PoemContainer />
+      </div>
     </div>
   );
 }
