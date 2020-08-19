a0ae11f9190996fd3777cbb08e315995168acf0e
karlmiche <karlymism@gmail.com>
2020-08-18 19:52:24 -0700

    got some more gsap animations happening on wtf.js

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -2,16 +2,18 @@ import React from 'react';
 import './App.css';
 import PoemContainer from './components/PoemContainer';
 import Title from './components/title';
-// import WhatIsThis from './components/Wtf';
+import WhatIsThis from './components/Wtf';
+import {BrowserRouter as Router, Route, Link, Redirect} from "react-router-dom";
+import Appbar from './components/Appbar';
+
 
 function App() {
   return (
-    <div>
       <div className="App">
+        <Appbar />
         <Title />
-        <PoemContainer />
-      </div>
-      {/* <WhatIsThis /> */}
+        <Route exact path='/poem' component={PoemContainer} />
+      <Route exact path='/explainer' component={WhatIsThis} />
     </div>
   );
 }
